b0f2e70a279f40e0230109d231f6ab3ef01eeaaa
Festus Ali <isfestus@gmail.com>
2022-06-21 00:30:36 +0100

    Add JS script console logger to GIT

---
+++ name_height_country.js
@@ -0,0 +1,7 @@
+// JavaScript code that prints name, height and country on the screen
+
+let name = "Festus Ali"
+let height = 6.0
+let country = "Nigeria"
+
+console.log("Fullname: " + name + "\nHeight: " + height + "\nCountry: " +  country)
